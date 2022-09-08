d125d3722daea8241f8653fb2755b4956b2fd5c7
Emmanuel Estallo <erestallo@up.edu.ph>
2022-09-08 18:25:12 +0800

    debug

--- RF/RLC_Networks/Impedance_Matching/pi_match.py
+++ RF/RLC_Networks/Impedance_Matching/pi_match.py
@@ -1,29 +1,29 @@
 from math import pi as pi, sqrt as sqrt 
 
-def L_match(q,r1,r2,dcblock,freq): 
-    if dcblock == False:
+def L_match(q,r1,r2,dcpass,freq): 
+    if dcpass == False:
+        if r2 > r1: 
+            l_load = r2/q 
+            L = l_load/(2*pi*freq)   
+            c_source = r1*q 
+            C = 1/(2*pi*freq*c_source)  
+        else: 
+            c_load = r2*q 
+            C = c_load/(2*pi*freq*c_load)
+            l_source = r1/q 
+            L = l_source/(2*pi*freq)   
+    
+    if dcpass == True: 
         if r2 > r1: 
             c_load = r2/q 
-            C = 1/(2*pi*freq*c_load)   
+            C = 1/(2*pi*freq*c_load)
             l_source = r1*q 
-            L = l_source/(2*pi*freq)  
+            L = l_source/(2*pi*freq) 
         else: 
             l_load = r2*q 
             L = l_load/(2*pi*freq)
             c_source = r1/q 
-            C = 1/(2*pi*freq*c_source)   
-    
-    if dcblock == True: 
-        if r2 > r1: 
-            l_load = r2/q 
-            L = l_load/(2*pi*freq)
-            c_source = r1*q 
-            C = 1/(2*pi*freq*c_source) 
-        else: 
-            c_load = r2*q 
-            C = 1/(2*pi*freq*c_load)
-            l_source = r1/q 
-            L = l_source/(2*pi*freq)
+            C = 1/(2*pi*freq*c_source)
     return L, C 
 
 rLoad = float(input("Enter load resistance (in ohms): "))
@@ -40,7 +40,7 @@ rv = max(rLoad,rSource)/(q_des**2 + 1)
 #get q_min 
 q_min = sqrt(min(rLoad,rSource)/rv - 1) 
 
-##### for dc block ##### 
+##### for dc pass ##### 
 if rSource < rLoad: 
     L1, C1 = L_match(q_min,rSource,rv,True,centerFreq) 
     L2, C2 = L_match(q_des,rv,rLoad,True,centerFreq) 
@@ -51,7 +51,27 @@ else:
     L2, C2 = L_match(q_min,rv,rLoad,True,centerFreq) 
     L_tot = L1 + L2  
 
-
+print("**************DC PASS VALUES**************")
 print(f"source cap: {C1}")
 print(f"load cap: {C2}")
-print(f"middle ind: {L_tot}")
+print(f"middle ind: {L_tot}") 
+
+##### for dc block ##### 
+if rSource < rLoad: 
+    L1, C1 = L_match(q_min,rSource,rv,False,centerFreq) 
+    L2, C2 = L_match(q_des,rv,rLoad,False,centerFreq) 
+    C_tot = 1/(1/C1 + 1/C2)
+
+else: 
+    L1, C1 = L_match(q_des,rSource,rv,False,centerFreq) 
+    L2, C2 = L_match(q_min,rv,rLoad,False,centerFreq) 
+    C_tot = 1/(1/C1 + 1/C2) 
+
+print("**************DC BLOCK VALUES**************")
+print(f"source ind: {L1}")
+print(f"load ind: {L2}")
+print(f"middle cap: {C_tot}") 
+
+
+print(f"At Q = {q_des}") 
+print("\n")
